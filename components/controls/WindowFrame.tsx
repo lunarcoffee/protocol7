ee53b8620bbf25af28c633ff5947e3756f69e4b2
lunarcoffee <lunarcoffee.pjc@gmail.com>
2025-07-20 19:14:55 -0400

    revamp window control buttons

--- components/controls/WindowFrame.tsx
+++ components/controls/WindowFrame.tsx
@@ -7,14 +7,47 @@ import { useWindowMove } from '@/hooks/useWindowMove';
 
 export interface WindowFrameProps extends PropsWithChildren {
   windowInfo: WindowInfo;
-  maximizable?: boolean;
-  // TODO: do we even need this here? what if this goes in processinfo? or eventually when we have
-  // multiple windows the window info
 }
+
+interface WindowControlButtonProps {
+  bgFrom: string;
+  bgTo: string;
+  ring: string;
+  topGlow: string;
+  topShadow: string;
+  bottomGlow: string;
+  onClick: () => void;
+}
+
+const WindowControlButton = ({
+  bgFrom,
+  bgTo,
+  ring,
+  topGlow,
+  topShadow,
+  bottomGlow,
+  onClick,
+}: WindowControlButtonProps) => (
+  <div
+    onClick={onClick}
+    className={`size-4 rounded-full bg-gradient-to-t ${bgFrom} ${bgTo} inset-shadow-sm inset-shadow-black/70 ring ${ring} shadow-sm shadow-white/80 overflow-clip`}
+  >
+    <div className="relative">
+      <div
+        className={`absolute w-6 h-3 -ml-1 -mt-1 rounded-full bg-radial ${topGlow} to-50% to-transparent`}
+      ></div>
+      <div
+        className={`absolute size-4 rounded-full inset-shadow-[0_1px_2px] ${topShadow}`}
+      ></div>
+      <div
+        className={`absolute w-4 h-4 mt-2 rounded-full bg-radial ${bottomGlow} to-70% to-transparent`}
+      ></div>
+    </div>
+  </div>
+);
 
 export const WindowFrame = ({
   windowInfo: { pid, wid, title, position, size, isOpen },
-  maximizable = true,
   children,
 }: WindowFrameProps) => {
   const moveWindow = useWindowMove();
@@ -23,8 +56,8 @@ export const WindowFrame = ({
   const onWindowDragStart = (initialX: number, initialY: number) => {
     const mouseMoveListener = (rawEvent: Event) => {
       const event = rawEvent as MouseEvent;
-      const deltaX = event.screenX - initialX;
-      const deltaY = event.screenY - initialY;
+      const deltaX = event.clientX - initialX;
+      const deltaY = event.clientY - initialY;
 
       moveWindow(wid, {
         x: position.x + deltaX,
@@ -52,21 +85,38 @@ export const WindowFrame = ({
     >
       <div
         className="flex flex-row items-center px-1"
-        onMouseDown={(event) => {
-          onWindowDragStart(event.screenX, event.screenY);
-        }}
+        onMouseDown={(event) => onWindowDragStart(event.clientX, event.clientY)}
       >
         <p className="pt-px text-sm">{title}</p>
         <div className="grow" />
         <div className="flex flex-row items-center gap-2">
-          <div className="size-4 rounded-full bg-gradient-to-b from-green-500 to-green-900 inset-shadow-xs inset-shadow-white/70 ring ring-green-950 shadow-sm shadow-aero-tint-darkest/70"></div>
-          <div className="size-4 rounded-full bg-gradient-to-b from-yellow-500 to-yellow-900 inset-shadow-xs inset-shadow-white/70 ring ring-yellow-950 shadow-sm shadow-aero-tint-darkest/70"></div>
-          <div
+          <WindowControlButton
+            bgFrom="from-lime-500"
+            bgTo="to-lime-900"
+            ring="ring-lime-950"
+            topGlow="from-lime-200"
+            topShadow="inset-shadow-green-950"
+            bottomGlow="from-lime-300"
             onClick={() => destroyWindow(pid, wid)}
-            className="size-4 rounded-full bg-gradient-to-b from-red-500 to-red-900 inset-shadow-xs inset-shadow-white/70 ring ring-red-950 shadow-sm shadow-aero-tint-darkest/70"
-          >
-            <div className=""></div>
-          </div>
+          />
+          <WindowControlButton
+            bgFrom="from-yellow-400"
+            bgTo="to-yellow-800"
+            ring="ring-yellow-950"
+            topGlow="from-yellow-200"
+            topShadow="inset-shadow-green-950"
+            bottomGlow="from-yellow-300"
+            onClick={() => destroyWindow(pid, wid)}
+          />
+          <WindowControlButton
+            bgFrom="from-red-500"
+            bgTo="to-red-900"
+            ring="ring-red-950"
+            topGlow="from-red-200"
+            topShadow="inset-shadow-red-950"
+            bottomGlow="from-red-300"
+            onClick={() => destroyWindow(pid, wid)}
+          />
         </div>
       </div>
       <div className="grow mt-1 bg-gray-200 rounded-sm border border-aero-tint-darkest/85 shadow-[0_0_2px] shadow-white/80">
